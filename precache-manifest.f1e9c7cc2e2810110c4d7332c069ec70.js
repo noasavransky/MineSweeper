8099662d357e5826b420f7308873280e9c91359b
Noa Savransky <noasavransky@gmail.com>
2020-05-06 16:39:12 +0300

    getting the build files to the main folder

---
+++ precache-manifest.f1e9c7cc2e2810110c4d7332c069ec70.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140ab1509d9820b0adce2b50e5bf0de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7088c1aa94d8447bdbc",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "c7eafa4edc6d377bcd1f",
+    "url": "/static/css/main.1cd3b67e.chunk.css"
+  },
+  {
+    "revision": "f7088c1aa94d8447bdbc",
+    "url": "/static/js/2.3488cda7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3488cda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7eafa4edc6d377bcd1f",
+    "url": "/static/js/main.99889e22.chunk.js"
+  },
+  {
+    "revision": "1e814049eb6d0e6e4b59",
+    "url": "/static/js/runtime-main.5f3b2643.js"
+  },
+  {
+    "revision": "7f3f75b96a2728ac2e0d8b356e814e72",
+    "url": "/static/media/genie.7f3f75b9.png"
+  },
+  {
+    "revision": "32e9f059e5c066391ef2073cba501c24",
+    "url": "/static/media/go.32e9f059.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68bb66a746d6bb34a22c527bd9aa4eb5",
+    "url": "/static/media/win.68bb66a7.png"
+  }
+]);
