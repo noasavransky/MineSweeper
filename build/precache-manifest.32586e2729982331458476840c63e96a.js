6ae14627998488d562747063c5123c9049c414b4
Noa Savransky <noasavransky@gmail.com>
2020-05-06 18:15:32 +0300

    readding the build after changes

---
+++ build/precache-manifest.32586e2729982331458476840c63e96a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418638d42591ddaf75042d7ff0b1c09f",
+    "url": "/MineSweeper/index.html"
+  },
+  {
+    "revision": "e6004f1418d5359dbfd1",
+    "url": "/MineSweeper/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "a75e834959103383367f",
+    "url": "/MineSweeper/static/css/main.1dc69e7a.chunk.css"
+  },
+  {
+    "revision": "e6004f1418d5359dbfd1",
+    "url": "/MineSweeper/static/js/2.eb3efaaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MineSweeper/static/js/2.eb3efaaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75e834959103383367f",
+    "url": "/MineSweeper/static/js/main.faca7ab1.chunk.js"
+  },
+  {
+    "revision": "1286b79baf6eb73fa672",
+    "url": "/MineSweeper/static/js/runtime-main.756b08d4.js"
+  },
+  {
+    "revision": "7f3f75b96a2728ac2e0d8b356e814e72",
+    "url": "/MineSweeper/static/media/genie.7f3f75b9.png"
+  },
+  {
+    "revision": "32e9f059e5c066391ef2073cba501c24",
+    "url": "/MineSweeper/static/media/go.32e9f059.png"
+  },
+  {
+    "revision": "68bb66a746d6bb34a22c527bd9aa4eb5",
+    "url": "/MineSweeper/static/media/win.68bb66a7.png"
+  }
+]);
